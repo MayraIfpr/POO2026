eef519345e223bd8a840023c92aa9451981a9df5
unknown <20241pvai0030030@estudantes.ifpr.edu.br>
2026-03-10 09:14:52 -0300

    Automazindo envio ao GitHub

---
+++ exerc_01.py
@@ -0,0 +1,10 @@
+# Desafio aula 
+alunos = [""] * 5
+
+for cont in range (0, len(alunos)):
+    alunos[cont] = (input("Nome do aluno: "))
+
+for cont in range (0, len(alunos)):
+    print("Aluno ", cont+1, ": ", alunos[cont])
+
+
---
+++ github.py
@@ -0,0 +1,25 @@
+import os
+
+print("✉️ configurando email....")
+comando_email = "git config user.email '20241pvai0030030@estudantes.ifpr.edu.br"
+os.system(comando_email)
+
+print("🆕 Adicionando modificaçôes...")
+comando01 = "git add *"
+os.system(comando01)
+
+mensagem = input("💬Mensagem do commit: ")
+
+while(len(mensagem) < 5):
+    print("❗Mensagem muito pequena, detalhe mais...")
+    mensagem = input("💬Mensagem do commit: ")
+
+print("✅ Registrando alterações...")
+comando02 = f"git commit -m'{mensagem}'"
+os.system(comando02)
+
+print("🔁Enviando projeto ao GitHub")
+comando03 = "git push"
+os.system(comando03)
+
+

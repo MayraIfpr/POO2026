78065977393a6626de84f428ae9167c795e814b9
unknown <20241pvai0030030@estudantes.ifpr.edu.br>
2026-06-02 10:10:19 -0300

    Arcade.jogo

--- Arcade/meu.jogo.py
+++ Arcade/meu.jogo.py
@@ -4,6 +4,28 @@ ALTURA=600
 LARGURA=800
 TITULO="Meu jogo"
 
+class Moeda(arcade.Sprite):
+    def __init__(self):
+        super().__init__("moeda.png", scale=0.10)
+
+    def update(self, delta_time):
+       self.center_x += self.change_x
+       self.center_y += self.change_y
+ 
+       if self.center_x > 800:
+           self.change_x 
+
+       elif self.center_x < 0:
+            self.change_x = 0
+
+       if self.center_y > 600:
+            self.change_y = 0 
+
+       elif self.center_y < 0:
+            self.change_y = 0    
+
+
+
 #personagem
 class Player(arcade.Sprite):
     def __init__(self):
@@ -14,25 +36,55 @@ class Player(arcade.Sprite):
     
     #atualizar personagem
     def update(self, delta_time):
-       pass
+       self.center_x += self.change_x
+       self.center_y += self.change_y
+
+       if self.change_x > 0:
+           self.texture = self.textura_direita
+
+       elif self.change_x <0:
+            self.exture = self.textura_esquerda
+        
+       
+           
 
 class JanelaJogo(arcade.Window):
     def __init__(self):
          super().__init__(LARGURA, ALTURA, TITULO)
          arcade.set_background_color(  arcade.color.AMAZON  )
 
+         self.movimento = 2
+
          self.personagem = Player()
+         self.moeda = Moeda()
 
          self.personagem.center_x = 400
          self.personagem.center_y = 300
 
+         self.moeda.center_x = 500
+         self.moeda.center_y = 300
+
+         self.moeda.center_x += self.movimento
+         self.moeda.center_y += self.movimento
+
+
+         
+         self.sprite_personagem = arcade.SpriteList()
+         self.sprite_personagem.append(self.personagem)
+
+         self.sprite_moedas = arcade.SpriteList()
+         self.sprite_moedas.append(self.moeda)
+
     def on_draw(self):
         self.clear()
+        self.sprite_personagem.draw()
+        self.sprite_moedas.draw()
 
-        arcade.draw_sprite(self.personagem)
+
 
     def on_update(self, delta_time):
-        pass
+        self.sprite_personagem.update(delta_time)
+        self.sprite_moedas.update(delta_time)
     
 def main():
     jogo = JanelaJogo()
